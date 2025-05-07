9e915449823e301817514112959d05f36555c25d
qvantt <2275399@gmail.com>
2025-05-07 15:11:20 +0300

    Скрытие TELEGRAM_BOT_TOKEN в файле .env и обновление start.sh и .gitignore

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 # Activate virtual environment
 source venv/bin/activate
 
-# Set environment variables
-export TELEGRAM_BOT_TOKEN="7857271142:AAHBTN1yvpoKIIrrmdXyV691xxs-qZhR9g0"
+# Load environment variables from .env file
+source .env
 
 # Run the bot
-python bot.py 
+python bot.py
